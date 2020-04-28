94bca8480204b142566562b8df9ed6d0f71b7d09
dewlyer <dewlyer@gmail.com>
2020-04-28 11:14:36 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,46 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+
+    componentDidMount() {
+        this.timerID = window.setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({date: new Date()});
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.date.toLocaleTimeString()}
+            </div>
+        );
+    }
+}
+
+const name = 'World';
+const element = (
+    <div>
+        <h1>Hello {name}</h1>
+        <Clock/>
+        <App/>
+    </div>
+);
+
+ReactDOM.render(element, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
