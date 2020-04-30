7541e94f9dc86034bac141e08b6c25daec143ff4
dewlyer <dewlyer@gmail.com>
2020-04-30 11:24:06 +0800

    update

--- src/views/monitor.jsx
+++ src/views/monitor.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './monitor.css';
 import {AppleOutlined, AndroidOutlined} from '@ant-design/icons';
 import {Layout, PageHeader, Tabs} from 'antd';
 import ListDone from './list-done';
@@ -9,7 +10,8 @@ export default class extends React.Component {
       height: 'auto'
     },
     contentStyle: {
-      padding: '0px 50px'
+      margin: '0 50px',
+      padding: '15px 25px'
     }
   };
 
@@ -21,7 +23,9 @@ export default class extends React.Component {
       <>
         <Layout>
           <Header style={headStyle}>
-            <PageHeader className="site-page-header" title="Title" subTitle="This is a subtitle"/>
+            <PageHeader className="site-page-header"
+                        title="云端识别监控"
+                        subTitle="A佳教育校园版云端识别监控系统"/>
           </Header>
           <Content style={contentStyle}>
             <Tabs defaultActiveKey="2">
@@ -29,15 +33,14 @@ export default class extends React.Component {
                 <ListDone/>
               </TabPane>
               <TabPane key="2" tab={<span><AndroidOutlined/> 排队中：3</span>}>
-                排队中
+                <ListDone/>
               </TabPane>
               <TabPane key="3" tab={<span><AndroidOutlined/> 已完成:4</span>}>
-                已完成
+                <ListDone/>
               </TabPane>
             </Tabs>
           </Content>
-          <Footer>
-          </Footer>
+          <Footer style={{textAlign: 'center'}}>Copyright &copy; 2015-2020 Ajia.cn All Rights Reserved.</Footer>
         </Layout>
       </>
     );
