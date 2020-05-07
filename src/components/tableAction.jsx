75669faf7d676506eefa94720d46a50cd52bdc42
dewlyer <dewlyer@gmail.com>
2020-05-07 14:15:09 +0800

    update

--- src/components/tableAction.jsx
+++ src/components/tableAction.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 import {Popconfirm, Button, message} from 'antd';
 import {updateMonitorPriority} from '../api/cloudRecognition';
 
-function handlerPriorityUpdate(projectId, subjectCode) {
-  return updateMonitorPriority({projectId, subjectCode});
-}
-
-function handlerPopConfirm(attrs, reload) {
-  const {projectId, subjectCode} = attrs;
-  handlerPriorityUpdate(projectId, subjectCode)
-    .then(() => {
-      message.success('优先级设置成功');
-      reload();
-    })
-    .catch(err => {
-      console.log(err);
-      message.error(err);
-    });
-}
-
-function handlerPopCancel() {
-  message.info('取消设置优先级');
-}
-
 const Action = (props) => {
   const {attrs, reload} = props;
+
+  const handlerPriorityUpdate = (projectId, subjectCode) => {
+    return updateMonitorPriority({projectId, subjectCode});
+  };
+
+  const handlerPopConfirm = async (attrs, reload) => {
+    const {projectId, subjectCode} = attrs;
+    try {
+      await handlerPriorityUpdate(projectId, subjectCode);
+      message.success('优先级设置成功');
+      reload();
+    } catch (err) {
+      console.log(err);
+      message.error(err);
+    }
+  };
+
+  const handlerPopCancel = () => {
+    message.info('取消设置优先级');
+  };
+
   return (
     <Popconfirm title="你确认要设置优先级？" okText="确认" cancelText="取消" placement="topRight"
                 onConfirm={() => handlerPopConfirm(attrs, reload)}
