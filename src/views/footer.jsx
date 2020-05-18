05222b49167fa60b1b9963a0f3e28ba8ad4b8729
dewlyer <dewlyer@gmail.com>
2020-05-18 16:08:24 +0800

    update

--- src/views/footer.jsx
+++ src/views/footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Layout} from 'antd';
 
-export default class Footer extends React.Component {
-  render() {
-    const {Footer} = Layout;
-    return <Footer style={{textAlign: 'center'}}>Copyright &copy; 2015-2020 Ajia.cn All Rights Reserved.</Footer>;
-  };
+function Footer() {
+  const {Footer} = Layout;
+  return (
+    <Footer style={{textAlign: 'center'}}>
+      Copyright &copy; 2015-2020 Ajia.cn All Rights Reserved.
+    </Footer>
+  );
 }
+
+export default Footer
