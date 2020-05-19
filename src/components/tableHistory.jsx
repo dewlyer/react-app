ae078fca1c8c403af38b8997cfcc26cc7ca388ba
dewlyer <dewlyer@gmail.com>
2020-05-19 15:07:30 +0800

    update

--- src/components/tableHistory.jsx
+++ src/components/tableHistory.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Tag, message} from 'antd';
 import {getMonitorHistory} from '../api/cloudRecognition';
+
+const getHistoryList = async (projectId, subjectCode, key) => {
+  const {data} = await getMonitorHistory({projectId, subjectCode});
+  const list = data ? data['recognitionList'] : [];
+  list.forEach((item, index) => item.key = `${key}_${index}`);
+  return list
+};
 
 const History = (props) => {
   const {attrs, child} = props;
 
-  const handlerHistoryList = (projectId, subjectCode) => {
-    return getMonitorHistory({projectId, subjectCode});
-  };
-
   const handlerHistoryClick = async () => {
-    const {projectId, subjectCode, key} = attrs;
     try {
-      const {data} = await handlerHistoryList(projectId, subjectCode);
-      const list = data ? data['recognitionList'] : [];
-      list.forEach((item, index) => item.key = `${key}_${index}`);
-      child(key, list);
+      const {projectId, subjectCode, key} = attrs;
+      const list = await getHistoryList(projectId, subjectCode, key);
+      if (typeof child === 'function') {
+        child(key, list);
+      }
     } catch (err) {
-      console.log(err);
       message.error(err);
     }
   };
 
-  const {statusWhenFinish, statusWhenStart} = attrs;
+  const {statusWhenStart, statusWhenFinish} = attrs;
+  const [start] = useState(statusWhenStart);
+  const [finish] = useState(statusWhenFinish);
 
   return attrs.children ? (
     <Button type="primary" size="small" shape="round" danger onClick={handlerHistoryClick}>
@@ -30,9 +34,9 @@ const History = (props) => {
     </Button>
   ) : (
     <span>
-      <Tag color="green" style={{margin: '1px'}}>开始动作: {statusWhenStart}</Tag>
+      <Tag color="green" style={{margin: '1px'}}>开始动作: {start}</Tag>
       <br/>
-      <Tag color="red" style={{margin: '1px'}}>结束动作: {statusWhenFinish}</Tag>
+      <Tag color="red" style={{margin: '1px'}}>结束动作: {finish}</Tag>
     </span>
   );
 };
